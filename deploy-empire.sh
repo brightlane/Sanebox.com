b85657a6ab3e8d1f30f87f0d44db7a1d7d796124
brightlane <benitopalmarini@aol.com>
2026-04-23 12:10:50 -0400

    Update deploy-empire.sh

--- deploy-empire.sh
+++ deploy-empire.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-echo "🚀 DEPLOYING SANEB OX EMPIRE (50K PAGES)"
-npm install
-node claudio-factory.js
-git add . -A
-git commit -m "SaneBox Empire: 50k pages LIVE $(date)" || echo "No changes"
-git push origin main
-echo "🎉 LIVE: https://brightlane.github.io/claudio-sanebox/"
+deploy-empire.sh
